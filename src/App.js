bdef1a5bb9530a03b31dbc16e8eb2a07d04de159
Nithya Radhakrishnan <nithya@RKs-MacAir-M2.local>
2023-06-16 21:10:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
+
+	const [calc,setCalc] = useState("");
+	const [result,setResult] = useState("");
+
+	const ops = ['/','*','+','-','.'];
+
+	const updatecal = value => {
+		if(
+			ops.includes(value) && calc === '' || ops.includes(value) && ops.includes(calc.slice(-1))
+		)
+		{
+			return;
+		}
+		setCalc(calc + value);
+		if(!ops.includes(value)) {
+			setResult(eval(calc + value).toString());
+		}
+	}
+
+	const CreateDigits = () => {
+
+		const digits = [];
+		for(let i=1;i<10;i++) {
+			digits.push(
+				<button onClick={ () => updatecal(i.toString()) } key={i}>{i}</button>
+			)
+		}
+		return digits;
+	}
+
+	const calculate = () => {
+		setCalc(eval(calc).toString());
+	}
+
+	const deletelast = () => {
+		if(calc == '') {
+			return;
+		}
+		const val = calc.slice(0,-1);
+		setCalc(val);
+	}
+	
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	  <div className="calculator">
+		<div className='display'>
+			 
+			{calc || "0"}<br/>
+			{result ? <span>{result}</span> : ''}
+		</div>
+		<div className='operators'>
+			<button onClick={ () => updatecal('/') }>/</button>
+			<button onClick={ () => updatecal('*') }>*</button>
+			<button onClick={ () => updatecal('+') }>+</button>
+			<button onClick={ () => updatecal('-') }>-</button>
+			<button onClick={deletelast}>DEL</button>
+		</div>
+		<div className='digits'>
+			{CreateDigits()}
+			<button onClick={ () => updatecal('0') }>0</button>
+			<button onClick={ () => updatecal('.') }>.</button>
+			<button onClick={calculate}>=</button>
+		</div>
+	  </div>
     </div>
   );
 }
